a20fbd02c9017bc0f1695e39e49966d3beae2ce8
Aminul Islam <amin13522@gmail.com>
2018-03-24 22:44:13 -0400

    new commit background and text

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
 
 
-(function(){
-  'use strict';
-
-  class Menu {
-    constructor(settings) {
-      this.nodeMenu = settings.nodeMenu;
-      settings.nodeMenuButton.addEventListener('click', this.toggle.bind(this));
-    }
-
-    toggle() {
-      return this.nodeMenu.classList.toggle('js-menu_activated');
-    }
-  }
-
-  let nodeMenu = document.querySelector('body');
-  
-  new Menu({
-    nodeMenu: nodeMenu,
-    nodeMenuButton: nodeMenu.querySelector('.js-menu__toggle')
-  });
-})();
 
